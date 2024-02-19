ba74970644387571315990bd4afdfda9123d7651
Jon Scheaffer <J_Schea29@yahoo.com>
2024-02-19 08:59:28 -0800

    First Commit

---
+++ jons_time_series_functions.py
@@ -0,0 +1,382 @@
+import cudf
+import cupy as cp
+import pandas
+import numpy as np
+
+# Data Preprocessing 
+from cuml.preprocessing import MinMaxScaler, StandardScaler, SimpleImputer, LabelEncoder, OneHotEncoder
+from cuml.compose import make_column_transformer
+from statsmodels.tsa.deterministic import CalendarFourier
+
+# Torch
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torch.utils.data import TensorDataset, DataLoader
+
+device = "cuda" if torch.cuda.is_available() else "cpu"
+
+
+class DeterministicProcess_gpu:
+    def __init__(self, order, constant=True, fourier=[]):
+                 
+        self.fourier = fourier
+        self.order = order
+        self.constant = constant
+                 
+    def in_sample(self, data):
+                 
+        data = data.copy()   
+                 
+        if self.constant:
+            data["constant"]=1
+        
+        data["trend"] = cp.arange(1,len(data.index)+1)
+        
+        self.last_value =len(data.index)+1         
+        
+        for i in range(2, self.order+1):
+                 data[f"trend_{i}"] = data["trend"]**i
+                 
+        if self.fourier:    
+            cf = cudf.DataFrame.from_pandas(CalendarFourier(freq=self.fourier[0], order=self.fourier[1]).in_sample(data.index.to_pandas()))
+                 
+            data = data.merge(cf, on="date")         
+        return data.sort_index()
+    def out_of_sample(self, data):
+        
+        data = data.copy()   
+                 
+        if self.constant:
+            data["constant"]=1
+        
+        data["trend"] = cp.arange(self.last_value,len(data.index)+self.last_value)
+                 
+        for i in range(2, self.order+1):
+                 data[f"trend_{i}"] = data["trend"]**i
+                 
+        if self.fourier:    
+            cf = cudf.DataFrame.from_pandas(CalendarFourier(freq=self.fourier[0], order=self.fourier[1]).in_sample(data.index.to_pandas()))
+                 
+            data = data.merge(cf, on="date")         
+        return data.sort_index()
+
+
+
+class ColTransformer_gpu:
+    def __init__(self, transformers):
+        self.transformers = transformers
+    
+    def fit_transform(self, X):
+        # Initialize an empty list to store transformed features
+        X = X.copy()
+        transformed_features = []
+        
+        num = 0
+        
+        # Iterate over each transformer and apply fit_transform
+        for name, transformer in self.transformers:
+            X_transformed = cudf.DataFrame(transformer.fit_transform(X[name]))
+
+            prev_num = num
+            num += len(X_transformed.columns)
+            
+            # Rename columns to ensure uniqueness
+            X_transformed.columns = [f"{i}" for i in range(prev_num, num)]
+            
+            transformed_features.append(X_transformed)  # Append transformed feature DataFrame
+
+            X  = X.drop(name, axis=1)
+
+        transformed_features.append(X)
+        
+        # Concatenate transformed features horizontally
+        X_transformed_concat = cudf.concat(transformed_features, axis=1)
+        X_transformed_concat.columns = [f"{i}" for i in range(len(X_transformed_concat.columns))]
+        
+        return X_transformed_concat
+    
+    def transform(self, X):
+        X = X.copy()
+        # Initialize an empty list to store transformed features
+        transformed_features = []
+        
+        num = 0
+        
+        # Iterate over each transformer and apply transform
+        for name, transformer in self.transformers:
+            X_transformed = cudf.DataFrame(transformer.transform(X[name]))
+
+            prev_num = num
+            num += len(X_transformed.columns)
+            
+            # Rename columns to ensure uniqueness
+            X_transformed.columns = [f"{i}" for i in range(prev_num, num)]
+            
+            transformed_features.append(X_transformed)  # Append transformed feature DataFrame
+            
+            X  = X.drop(name, axis=1)
+
+        transformed_features.append(X)
+        
+        # Concatenate transformed features horizontally
+        X_transformed_concat = cudf.concat(transformed_features, axis=1)
+        X_transformed_concat.columns = [f"{i}" for i in range(len(X_transformed_concat.columns))]
+        
+        return X_transformed_concat
+
+
+
+class Prepare_data:
+    '''
+    A Class for preparing data for Hybrid Models. 
+    
+    Parameters:
+        X_1_column: Columns to be used in the 1st Machine Learning model,
+        unwanted_columns: Columns to not be used for the 2nd Machine Learning Model.
+                 
+    Attributes:
+        X_1(X): Creates data for the first model,
+        X_2(X): Creates data for the second model,
+        preprocessor_2: Preprossesing for data of type oj=bject, catergory, and ,
+        transform(X): Transform data performing X_1(X) and X_2(X)  
+    '''
+    
+    def __init__(self, X_1_column, transformer_list, unwanted_columns=[], to_tensor=False):
+        '''
+        Initializes the Prepare_data class.
+        
+        Parameters:
+            X_1_column: Columns to be used in the 1st Machine Learning model,
+            unwanted_columns: Columns to not be used for the 2nd Machine Learning Model.
+        '''
+        
+        #Defining instance variables
+        self.column_list = X_1_column
+        self.unwanted_columns = unwanted_columns
+        self.to_tensor = to_tensor
+        self.ct = ColTransformer_gpu(transformer_list)
+        self.dpg = DeterministicProcess_gpu(1, fourier = ["W", 2])
+        
+    def X_1_fit_transform(self, X): 
+        '''
+        Transform data into X_1. Expects linear 1st model so uses Deterministic Process. 
+        
+        Parameters:
+            X: Data for the model. 
+        '''
+        
+        # return deterministic process
+        return self.dpg.in_sample(X[self.column_list][~X.index.duplicated(keep='last')])
+
+    def X_1_transform(self, X): 
+        '''
+        Transform data into X_1. Expects linear 1st model so uses Deterministic Process. 
+        
+        Parameters:
+            X: Data for the model. 
+        '''
+        
+        return self.dpg.out_of_sample(X[self.column_list][~X.index.duplicated(keep='last')])
+
+    def X_2_fit_transform(self, X):
+        '''
+        Transform data into X_2.
+        
+        Parameters:
+            X: Data for the model. 
+        '''
+        X_2 = X.reset_index()
+        X_2 = X_2.drop(self.column_list + self.unwanted_columns + ["date"], axis=1)
+        
+        X_2 = self.ct.fit_transform(X_2)
+        return X_2
+        
+    def X_2_transform(self, X):
+        '''
+        Transform data into X_2.
+        
+        Parameters:
+            X: Data for the model. 
+        '''
+        X_2 = X.reset_index()
+        X_2 = X_2.drop(self.column_list + self.unwanted_columns + ["date"], axis=1)
+        X_2 = self.ct.transform(X_2)
+        
+        return X_2
+    
+    def fit_transform(self, X):
+        '''
+        Transform data into X_1 and X_2.
+        
+        Parameters:
+            X: Data for the model. 
+        '''
+        X_1, X_2 = self.X_1_fit_transform(X), self.X_2_fit_transform(X)
+        if self.to_tensor:
+            
+            X_1 = torch.tensor(X_1.values).to(device)
+            X_2 = torch.from_numpy(X_2.astype("float")).to(device)
+        
+        return X_1, X_2
+        
+    def transform(self, X, to_tensor=False):
+        '''
+        Transform data into X_1 and X_2.
+        
+        Parameters:
+            X: Data for the model.
+            to_tensor: If true changes data to tensors on the GPU.
+        '''
+        X_1, X_2 = self.X_1_transform(X), self.X_2_transform(X)
+        
+        if self.to_tensor:
+            
+            X_1 = torch.tensor(X_1.values).to(device)
+            X_2 = torch.from_numpy(X_2.astype("float")).to(device)
+            
+        return X_1, X_2
+
+class Hybrid_Time_Series_ML:
+    '''
+    A Class for creating either boosted or stacked Hybrid Machine Learning Models for Time Series. 
+    
+    Parameters:
+        model_1: 1st Machine Learning model to be used (usually linear),
+        model_2: 2nd Machine Learning Model to be used (usually non-linear),
+        Boosted: A boolean variable which, when True, trains 2nd model on the residuals of the 1st model. If false, 
+                 then it adds the predictions of the first model to the 2nd dataset. Default is Boosted.
+                 
+    Attributes:
+        ensure_as_dataframe(y_pred, X, y): A function used to ensure the final predictions are in the correct format.
+        stack_prediction(X_2, y_pred): Adds predictions from the first model to data for the 2nd model.
+        fit(X_1, X_2, y): Fits Hybrid model to the data.
+        predict(X_1, X_2): Generates Prediction for Hybrid model.
+        name(): Gives the names of both models. 
+    '''
+    
+    def __init__(self, model_1, model_2, Boosted=True):    
+        '''
+        Initializes the Hybrid_Time_Series_ML class.
+        
+        Parameters:
+            model_1: 1st Machine Learning model to be used (usually linear),
+            model_2: 2nd Machine Learning Model to be used (usually non-linear),
+            Boosted: A boolean variable which, when True, trains 2nd model on the residuals of the 1st model. If false 
+                     then it adds the predictions of the first model to the 2nd dataset. Default is Boosted.
+        '''
+
+        #Defining instance variables
+        self.model_1 = model_1
+        self.model_2 = model_2
+        self.boosted = Boosted
+
+    def change_model(self, model, first=True):
+        '''
+        Change input either model
+        
+        Parameters:
+            model: new model, 
+            first: determines which model to change.
+        '''
+        
+        if first:
+            self.model_1 = model
+        
+        else:
+            self.model_2 = model
+
+            
+    def fit(self, X_1, X_2, y):
+        '''
+        Fits Hybrid model to the data.
+        
+        Parameters:
+            X_1: The data to use for training the 1st model,
+            X_2: The data to use for training the 2nd model,
+            y: The target variable.
+        '''
+
+        y_c = y_train.copy()
+        y_c.set_index(["store_nbr", "family"], append=True, inplace=True)
+        
+        unstack_y = y_c.unstack(["store_nbr", "family"])
+
+        self.unstack_y = unstack_y
+        
+        self.model_1.fit(X_1_train, unstack_y)
+
+        y_fit = self.model_1.predict(X_1_train)
+
+        y_fit.columns = unstack_y.columns
+        y_fit.index = X_1.index
+        
+        if self.boosted:
+
+            y_resid = unstack_y - y_fit
+            
+            y_resid.columns = y_fit.columns
+            y_resid.index = X_1.index
+            
+            y_resid = y_resid.stack(["store_nbr", "family"]).reset_index().sort_values(["store_nbr", "family", "date"])
+
+            self.model_2.fit(X_2_train, y_resid["sales"])
+
+        else:
+
+            y_fit = y_fit.stack(["store_nbr", "family"]).reset_index().sort_values(["store_nbr", "family", "date"])
+            X_2 = X_2.copy()
+            X_2["fit"] = y_fit["sales"]
+
+            self.model_2.fit(X_2_train, y_c.reset_index()["sales"])
+            
+    def predict(self, X_1, X_2):
+        '''
+        Generates Prediction for Hybrid model.
+        
+        Parameters:
+            X_1: The data to use for making predictions with the 1st model,
+            X_2: The data to use for making predictions with  the 2nd model.
+        
+        Returns: Hybrids Models Predictions for data X_1 and X_2.
+        '''
+
+        y_pred_1 = self.model_1.predict(X_1)
+        
+        y_pred_1.columns = self.unstack_y.columns
+        y_pred_1.index = X_1.index
+
+        y_pred_1 = y_pred_1.stack(["store_nbr", "family"]).reset_index().sort_values(["store_nbr", "family", "date"])
+        
+        if self.boosted:
+
+            y_pred_2 = cudf.DataFrame(self.model_2.predict(X_2))
+
+            y_pred_1 = y_pred_1.set_index(["date", "store_nbr", "family"])
+            
+            y_pred_2.index = y_pred_1.index
+            y_pred_2.columns = y_pred_1.columns
+            
+            y_pred_boosted = y_pred_2 + y_pred_1
+            
+            return y_pred_boosted.clip(0)
+            
+        else:
+
+            X_2 = X_2.copy()
+            X_2["fit"] = y_pred_1["sales"]
+            
+            y_pred_stacked = cudf.DataFrame(xgb.predict(X_2))
+            
+            y_pred_1 = y_pred_1.set_index(["date", "store_nbr", "family"])
+            
+            y_pred_stacked.index = y_pred_1.index
+            y_pred_stacked.columns = y_pred_1.columns
+            
+            return y_pred_stacked.clip(0)     
+            
+    def name(self):
+        '''
+        Gives the names of both models. 
+        '''
+        return f"Model 1: {self.model_1.__class__.__name__}, Model 2: {self.model_2.__class__.__name__}"
